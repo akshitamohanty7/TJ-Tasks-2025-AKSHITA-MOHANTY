bb0572170c51fd0fa5ea379791a16aeb0b37bc67
akshitamohanty7 <akshitamohanty0@gmail.com>
2025-10-15 18:36:44 +0530

    Update task0.py

--- task0.py
+++ task0.py
@@ -1,13 +1,15 @@
 t = int(input())
 for _ in range(t):
     n = int(input())
+    
     a = list(map(int, input().split()))
     
     count_negative = a.count(-1)
     count_zero = a.count(0)
     
-    count = count_zero + 2 * count_negative
-    if count_negative % 2 == 1 and count_zero == 0:
-        count += 1
+    min_operations = count_zero
+    
+    if count_negative % 2 != 0:
+        min_operations += 2
         
-    print(count)
+    print(min_operations)
