8eacf55f31720b8eca3d1c02bcc055a32189a536
akshitamohanty7 <akshitamohanty0@gmail.com>
2025-10-15 17:48:28 +0530

    task1.py

---
+++ task1.py
@@ -0,0 +1,17 @@
+#task1
+
+num = int(input("Enter the number of operations: "))
+
+for _ in range(num):
+    x = int(input("Enter the x: "))
+    n = int(input("Enter the n: "))
+    
+    arr = []
+    for i in range(n):
+        if i % 2 == 0:
+            arr.append(-x)
+            
+        else:
+            arr.append(x)
+            
+    print(sum(arr))
